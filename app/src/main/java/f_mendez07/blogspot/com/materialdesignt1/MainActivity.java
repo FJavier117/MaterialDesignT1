736a4b305ac5d3b5c884b870484c2953d29c645e
FJavier117 <fjmendez07@gmail.com>
2020-02-01 22:00:08 -0600

    primer commit

---
+++ app/src/main/java/f_mendez07/blogspot/com/materialdesignt1/MainActivity.java
@@ -0,0 +1,14 @@
+package f_mendez07.blogspot.com.materialdesignt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
